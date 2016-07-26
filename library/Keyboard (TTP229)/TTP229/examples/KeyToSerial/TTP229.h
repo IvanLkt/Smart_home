b6ea3951e50cf3c0d8319c12f05bd118a458abab
Ivan Laktionov <wains777@gmail.com>
2016-07-26 14:42:40 +0500

    add some library

--- library/Keyboard (TTP229)/TTP229/examples/KeyToSerial/TTP229.h
+++ library/Keyboard (TTP229)/TTP229/examples/KeyToSerial/TTP229.h
@@ -24,46 +24,79 @@
 | POSSIBILITY OF SUCH DAMAGE.                                                  |
 \******************************************************************************/
 /************************************ USES ************************************\
-| This sketch demonstrates the use of TTP229 Module, in 16 Buttons Mode.       |
-| It displays the pressed number on the Terminal                               |
+| This library designed to communicate with the TTP229 chip  from an Arduino   |
 |                                                                              |
 | It works with a TTP229 (16-Channel Digital Touch Capacitive Switch Sensor)   |
 | using the 2-wires serial interface protocol - only 2 arduino pins.           |
 \******************************************************************************/
 /*********************************** CIRCUIT **********************************\
-| 16 Buttons Mode:                                                             |
+| General:                                                                     |
 |	* TTP229 VCC to pin VCC                                                    |
 |	* TTP229 GND to pin GND                                                    |
-|	* TTP229 SCL to pin 2                                                      |
-|	* TTP229 SDO to pin 3                                                      |
+|	* TTP229 SCL to any pin you choose...                                      |
+|	* TTP229 SDO to any pin you choose...                                      |
+|                                                                              |
+| 16 Buttons Mode (else 8 Buttons Mode):                                       |
 |	* TTP229 TP2 to GND via 1 Megohm resistor!                                 |
-|	# See TTP229_Modes.jpg for help...                                         |
+|	# Must use the ReadKey16, GetKeys16... else use the ReadKey8, GetKeys8...  |
+|                                                                              |
+| Multi Keys Mode (else Single key Mode):                                      |
+|	* TTP229 TP3 to GND via 1 Megohm resistor!                                 |
+|	* TTP229 TP4 to GND via 1 Megohm resistor!                                 |
+|                                                                              |
+| 64Hz Sampling Rate (else 8Hz Sampling Rate):                                 |
+|	* TTP229 TP5 to GND via 1 Megohm resistor!                                 |
+|                                                                              |
+| 2ms Wake Up Rate (else 4ms Wake Up Rate):                                    |
+|	* TTP229 TP6 to GND via 1 Megohm resistor!                                 |
+|                                                                              |
+| 60sec Key On Timeout (else No Key On Timeout):                               |
+|	* TTP229 TP7 to GND via 1 Megohm resistor!                                 |
 |                                                                              |
 | Important:                                                                   |
 |	* Must reconnect the TTP229 power so the mode changes will take effect     |
 |	* The 1 Megohm resistors already exist on some TTP229 modules              |
 \******************************************************************************/
 
-#include <TTP229.h>
+#if ARDUINO >= 100
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
 
-const int SCL_PIN = 2;  // The pin number of the clock pin.
-const int SDO_PIN = 3;  // The pin number of the data pin.
+#ifndef TTP229_h
+#define TTP229_h
 
-TTP229 ttp229(SCL_PIN, SDO_PIN); // TTP229(sclPin, sdoPin)
-
-void setup()
+class TTP229
 {
-	Serial.begin(9600);
-	Serial.println("Start Touching One Key At a Time!");
-}
+	public:
+		TTP229(uint8_t sclPin, uint8_t sdoPin);
+		uint8_t
+			ReadKey8(),
+			GetKey8(),
+			ReadKeys8(),
+			GetKeys8(),
+			ReadKey16(),
+			GetKey16();
+		uint16_t
+			ReadKeys16(),
+			GetKeys16();
+	private:
+		void
+			WaitForTouch(),
+			Key8(),
+			Keys8(),
+			Key16(),
+			Keys16();
+		bool
+			IsTouch(),
+			GetBit();
+			
+		uint8_t _sclPin, _sdoPin;
+		uint8_t _key8, _keys8, _key16;
+		uint16_t _keys16;
+};
 
-void loop()
-{
-	uint8_t key = ttp229.ReadKey16(); // Blocking
-	if (key) Serial.println(key);
-
-//	uint8_t key = ttp229.GetKey16(); // Non Blocking
-//	Serial.println(key);
-}
+#endif
 
 
